b138060ca13e0160f94b5b3512a6840c3e639fd3
michelhotz <michelhotz@gmail.com>
2022-03-22 21:18:44 -0300

    meu primeiro commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }  
+};
